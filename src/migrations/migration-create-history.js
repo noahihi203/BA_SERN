660a96a1a69e0bbce115fd298e3957d5e95ea63b
Lâm Bùi Quốc <22520733@ms.uit.edu.vn>
2024-08-26 19:05:52 +0700

    firt commit

---
+++ src/migrations/migration-create-history.js
@@ -0,0 +1,36 @@
+'use strict';
+
+//const sequelize = require("sequelize");
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('histories', { 
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      patientId: {
+        type: Sequelize.STRING
+      },
+      doctorId: {
+        type: Sequelize.STRING
+      },
+      descriptionId: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('histories');
+  }
+};
