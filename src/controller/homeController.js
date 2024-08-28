8a45aca1daa076530a2b355dd0b38ae37718c912
Lâm Bùi Quốc <22520733@ms.uit.edu.vn>
2024-08-28 20:51:36 +0700

    video 27 hoidanit SERN fullstack

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -7,8 +7,7 @@ let getHomePage = async (req, res) => {
         return res.render('homepage.ejs', {
             data: JSON.stringify(data)
         });
-    } catch (e)
-    {
+    } catch (e) {
         console.log(e)
     }
 }
@@ -34,10 +33,37 @@ let displayGetCRUD = async (req, res) => {
     })
 }
 
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id;
+
+    console.log(userId)
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+        return res.render('editCRUD.ejs', {
+            user: userData
+        });
+    }
+    else {
+        return res.send('User not found!');
+    }
+
+}
+
+let putCRUD = async(req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    });
+}
+
 module.exports = {
     getHomePage: getHomePage,
     getCRUD: getCRUD,
     postCRUD: postCRUD,
     displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
 }
 
